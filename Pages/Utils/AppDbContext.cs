98abaddee434949c14e1f06a393965fc7a546353
Faizur Rahman <faizur@angainfotech.in>
2025-07-08 18:58:08 +0530

    location ef related worked.

---
+++ Pages/Utils/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RFID2.Model;
+
+namespace RFID2.Pages.Utils
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Location> Locations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Location>()
+                .HasIndex(l => l.LocationName)
+                .IsUnique();
+
+            modelBuilder.Entity<Location>()
+                .Property(l => l.CreatedOn)
+                .HasDefaultValueSql("getdate()");
+        }
+    }
+}
